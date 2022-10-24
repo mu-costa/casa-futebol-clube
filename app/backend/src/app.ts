230a9febf9fd00bf2d6b59a754286f57a78c3454
Murilo Costa <mcosta12358@gmail.com>
2022-10-24 13:20:25 -0300

    feat: req03

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -10,6 +10,7 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.post('/login', (req, res) => res.sendStatus(200));
   }
 
   private config():void {
